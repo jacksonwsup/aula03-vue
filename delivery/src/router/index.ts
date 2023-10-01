e54832ddd5d82be8296eeac36eb7dbcf0fd2b93f
Jackson R <jacksonwsup@gmail.com>
2023-09-30 23:37:24 -0300

    Criando Rotas

---
+++ delivery/src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter,createWebHistory } from "vue-router"; 
+
+import HomeView from "../views/home/HomeView.vue";
+import PedidoView from "../views/pedido/PedidoView.vue";
+import NotFoundView from "../views/home/NotFoundView.vue";
+import path from "path";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL), //Esta é a base da navegação
+    routes: [
+        {
+            path: "/",
+            name:"home",
+            component: HomeView,
+        },
+        {
+            path: "/pedido",
+            name:"pedido",
+            component: PedidoView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name:"notfound",
+            component: NotFoundView,
+        }
+        
+    ]
+})
+
+export default router;
+
+
